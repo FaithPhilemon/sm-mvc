71a87684d680d3d8cec5118e331326288551eb67
Faith Philemon <philemonfaith@gmail.com>
2019-05-30 11:38:05 +0100

    autoload core libs

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,9 +3,7 @@
 require_once 'config/config.php';
 
 
-// LOAD LIBRARIES
-require_once 'libraries/Controller.php';
-require_once 'libraries/Core.php';
-require_once 'libraries/Database.php';
-
-
+// Autoload core libraries
+spl_autoload_register(function($className){
+    require_once 'libraries/'.$className.'.php';
+});
